c26b8ff9f7eba776d059a1487cfae3c3d1c6628e
Damian Matkowski <getrasa4@gmail.com>
2025-10-30 17:31:10 +1000

    New icon, title, and other meta

--- storefront/src/app/(main)/layout.tsx
+++ storefront/src/app/(main)/layout.tsx
@@ -7,6 +7,18 @@ import { NavigationBar } from "~/components/ui/navigation";
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
+  title: "Saunas of the World",
+  description: "Favourite Australian sauna, ice baths and heaters manufacturer.",
+  icons: {
+    icon: "/favicon.png",
+  },
+  openGraph: {
+    images: [
+      {
+        url: "https://assets-global.website-files.com/63ee5237d63284147aced937/643e7bf6b64e2161c21695e0_63f5977f207f7c1827a8e644_how%2520many%2520calories%2520burned%2520in%2520sauna%25202.jpeg",
+      },
+    ],
+  },
 }
 
 export default function MainLayout({
